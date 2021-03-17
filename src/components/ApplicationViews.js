dd5f294cae649c30f900b3582dea1589d249389b
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-17 11:23:03 -0500

    fix: creating containers for homepage and structuring html for flex

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { Home } from "./Home"
+import { Home } from "./home/Home"
 
 export const ApplicationViews = () => {
     return (
---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css"
+
+export const Home = () => (
+    <>
+        <section className="topFIVEhomepage">
+            <h1>Welcome to topFIVE</h1>
+            <article className="profileCard">
+                This will be the users profile card
+            </article>
+            <main className="mainFeed">
+                This will be the Main Feed
+            </main>
+            <aside className="createNewtopFIVEcard">
+                This will be the create new topFIVE card
+            </aside>
+        </section>
+    </>
+)
