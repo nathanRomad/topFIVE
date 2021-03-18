1f8c8891fa7dfc0de6a9416d07061fedc131ace4
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-18 14:20:24 -0500

    fix: setup TopFiveCard as react.bootstrap card style

--- src/components/TopFive/TopFiveCard.js
+++ src/components/TopFive/TopFiveCard.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Card } from 'react-bootstrap';
-
+import Card from 'react-bootstrap/Card';
 
 export const TopFiveCard = ({ topFive }) => (
     <Card style={{ width: '18rem' }}>
