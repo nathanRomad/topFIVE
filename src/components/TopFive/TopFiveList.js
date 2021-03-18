ea3195e9435c6982b22e6c011ff8548abd645ace
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-18 13:59:41 -0500

    fix: syntax error on state variable was causing data to come back undefined

--- src/components/TopFive/TopFiveList.js
+++ src/components/TopFive/TopFiveList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
+import { userStorageKey } from "../auth/authSettings";
 import { useHistory } from "react-router-dom" // import from libraries before your local modules
 import { TopFiveContext } from "../TopFive/TopFiveProvider";
 import { TopFiveCard } from "./TopFiveCard";
@@ -7,7 +8,8 @@ export const TopFiveList = () => {
     const { topFive, getTopFive, getTopFiveById, addTopFive, deleteTopFive, updateTopFive } = useContext(TopFiveContext)
     const history = useHistory()
 
-    const { userTopFive, setUserTopFive } = useState([])
+    const [ userTopFive, setUserTopFive ] = useState([])
+    console.log('userTopFive: ', userTopFive);
  
 
     useEffect(() => {
@@ -15,16 +17,22 @@ export const TopFiveList = () => {
         getTopFive()
     }, [])
 
+    useEffect(() => {
+        const currentUserId = parseInt(sessionStorage.getItem(userStorageKey))
+        const filteredTopFive = topFive.filter(topFive => topFive.userId === currentUserId)
+        setUserTopFive(filteredTopFive)
+    }, [topFive])
+
     return (
         <>
             <h2>topFIVE</h2>
-            {/* <div>
+            <div>
                 {
                     userTopFive.map(topFive => {
                         return <TopFiveCard key={topFive.id} topFive={topFive} />
                     })
                 },
-            </div> */}
+            </div>
         </>
     )
 }
