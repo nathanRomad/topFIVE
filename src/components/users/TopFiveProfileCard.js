e03a92d171736649b6b5412d6d8f4fb696d9a7cf
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-30 11:11:09 -0500

    fix: image upload saving to database.json, login & registration updated

--- src/components/users/TopFiveProfileCard.js
+++ src/components/users/TopFiveProfileCard.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
+import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
 // import '../../App.css'
 
 export const TopFiveProfileCard = ({ users }) => (
     <section className="topFIVEprofileCard">
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="" alt="" />
+            <Card.Img variant="top" src={users.imageUrl} />
+            {/* <Image cloudName="nateromad" publicId="" width="300" crop="scale" /> */}
             <Card.Body>
                 <Card.Title>{users.name}</Card.Title>
                 <Card.Text> {users.location}
