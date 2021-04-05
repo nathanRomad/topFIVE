d288092d08ffdf4e453fbf9b12587f92d303937d
nathanRomad <nathan.a.hamilton@gmail.com>
2021-04-05 18:48:48 -0500

    fix: updating about page / need image src to work

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,25 +1,25 @@
 import React from "react";
+// import { Nate } from "../images/nate.JPG";
+import "./About.css";
 
 export const About = () => (
     <>
         <section className="aboutContainer">
             <div className="about-section">
                 <h1>Welcome to topFIVE!</h1>
-                <p>I'm Nathan, a junior developer, and creator of topFIVE.</p>
+                <p>An app to create & follow topFIVE lists!</p>
+                <p>I'm Nate, a junior developer, and creator of topFIVE.</p>
             </div>
-            <h2 className="teamTitle" >Our Team</h2>
             <div className="row">
                 <div className="column">
                     <div className="card">
-                        {/* <img src="" alt=""> */}
-                            <div className="container">
-                                <h2>Nathan Hamilton</h2>
-                                <p className="title">Creator</p>
-                                <p>I am a jr. software developer on my way to going pro!</p>
-                                <p>nathan.a.hamilton@gmail.com</p>
-                                {/* <p><button className="button">Contact</button></p> */}
-                            </div>
-                        {/* </img> */}
+                        {/* <img src={Nate} alt="image"></img> */}
+                        <div className="container">
+                            <h2>Nathan Hamilton</h2>
+                            <p className="title">Creator</p>
+                            <p>I am a jr. software developer on my way to going pro!</p>
+                            <p>nathan.a.hamilton@gmail.com</p>
+                        </div>
                     </div>
                 </div>
             </div>
